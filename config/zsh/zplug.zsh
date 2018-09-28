01b9add07731760d17ac8300ace4a1544ff0a8e6
Pablo Olmos de Aguilera Corradini <pablo@odac.co>
2018-09-27 22:46:09 -0300

    Add prezto editor module

--- config/zsh/zplug.zsh
+++ config/zsh/zplug.zsh
@@ -1,4 +1,5 @@
 zplug 'zplug/zplug', hook-build:'zplug --self-manage'
+zplug 'modules/editor', from:prezto
 zplug "$ZDOTDIR", from:local, use:"*.zsh"
 zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search", defer:3
