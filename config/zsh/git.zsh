0e7b45d0a6bdfa4281231377809570e106c5bd57
Pablo Olmos de Aguilera Corradini <pablo@odac.co>
2018-10-30 01:26:15 -0300

    zsh: fix some problems when using completion on `g` wrapper function

--- config/zsh/git.zsh
+++ config/zsh/git.zsh
@@ -1,17 +1,15 @@
+# because I'm (too) lazy
+
 # Use hub (http://hub.github.com/) if available
 if (( $+commands[hub] )); then
-  alias g=git
   alias git=hub
-  # compdef g=hub
-  compdef hub=git
 else
   compdef g=git
 fi
 
 # No arguments: `git status`
 # With arguments: acts like `git`
-unalias g
-function g() {
+function _g() {
   if [[ $# > 0 ]]; then
     git $@
   else
@@ -20,12 +18,19 @@ function g() {
   fi
 }
 
+# @url https://superuser.com/questions/1280348/zsh-autocompletion-breaks-when-i-add-a-wrapper-to-a-command
+alias g=_g
+compdef _g=git
+
+# Some aliases
+alias gia="git add --verbose" # maybe it should be done within git?
+
 # Allows commit message without typing quotes (can't have quotes in the commit msg though).
 # https://github.com/r00k/dotfiles/blob/master/zsh/functions#L44
-#unalias gc
 function gc {
   git commit -m "$*"
 }
 
 # Don't try to glob with zsh so git can do the right stuff
 # alias git='noglob git'
+
